f8b55c84d88cc13060232fa7cb5bf31c74d22b29
Rakesh K <rakeshdraft4@gmail.com>
2026-01-18 21:05:53 +0530

    Add files via upload

---
+++ logger.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+def setup_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    ch.setFormatter(formatter)
+    
+    logger.addHandler(ch)
+    return logger
